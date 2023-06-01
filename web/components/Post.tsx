1b033f67769e5a11baf511f3baf3c8b2f1681553
Ramiz Ahmed <ramizahmedar@gmail.com>
2023-06-02 04:47:41 +0500

    Add username, createdAt next to posts

--- web/components/Post.tsx
+++ web/components/Post.tsx
@@ -1,13 +1,17 @@
 import { Post as PostType } from "@/gql/graphqlcomponents";
+import { timeSince } from "@/utils/timeSincecomponents";
 
 export default function Post({ post }: { post: PostType }) {
   if (!post) return null;
   return (
-    <div className="mb-6 bg-primary rounded-sm p-5">
-      <h2 className="mb-4 text-2xl font-bold">{post.title}</h2>
-      <p>{post.textSnippet}</p>
-      <p>{post.user.username}</p>
-      <p>{post.user.email}</p>
+    <div className="mb-6 bg-primary rounded-sm p-3">
+      <p className="text-xsm mb-1">
+        <span>Posted by {post.user.username}</span>{" "}
+        <span className="pl-[3px]">{timeSince(post?.createdAt)}</span>
+      </p>
+
+      <h2 className="mb-2 text-lg font-bold">{post.title}</h2>
+      <p className="text-reg">{post.textSnippet}</p>
     </div>
   );
 }
