363adb2e7c389de4b9fdc15d937a19500919e26b
Ramiz Ahmed <ramizahmedar@gmail.com>
2023-06-07 14:50:55 +0500

    Fix logging

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,14 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-  ],
-});
-
-prisma.$on("query", async (e) => {
-  console.log(`${e?.query} ${e?.params}`);
+  log: ["query"],
 });
